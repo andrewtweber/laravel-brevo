5895ddd00abed2ef4b77e4096f3fa8d5a30370a8
andrewtweber <andrew@midwestscc.com>
2025-08-06 21:48:34 -0500

    phpstan fix

--- src/Facades/Brevo.php
+++ src/Facades/Brevo.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace Hofmannsven\Brevo\Facades;
 
+use Brevo\Client\Configuration;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * For some reason larastan is not detecting these Facade methods:
+ * 
+ * @method static Configuration getConfiguration()
+ * @method static void setConfiguration(Configuration $configuration)
+ */
 class Brevo extends Facade
 {
     protected static function getFacadeAccessor(): string
